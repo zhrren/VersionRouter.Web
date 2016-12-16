8acfa4ff8d691e2f6d38c9f489e70064c8843b3f
钟任 <zhrren@qq.com>
2016-12-16 16:22:52 +0800

    更新

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using VersionRouter.Web.Core;
 using NLog;
 using Mark.VersionRouter;
+using System;
+using Mark.ApiResult;
 
 namespace VersionRouter.Web.Controllers
 {
@@ -15,7 +17,7 @@ namespace VersionRouter.Web.Controllers
             _versionSettingsService = versionSettingsService;
         }
 
-        public IActionResult Index(string name="", string platform="", string vesion="1.0.0", string uid="", bool redirect=false)
+        public IActionResult Index(string name = "", string platform = "", string vesion = "1.0.0", string uid = "", bool redirect = false)
         {
             var packages = _versionSettingsService.GetPackages(name);
             var groups = _versionSettingsService.GetGroups();
@@ -23,10 +25,35 @@ namespace VersionRouter.Web.Controllers
             var item = router.Match(platform, vesion, uid);
 
             var url = item == null ? "" : item.Url;
-            if(redirect && !string.IsNullOrWhiteSpace(url))
+            if (redirect && !string.IsNullOrWhiteSpace(url))
                 return Redirect(item.Url);
             else
                 return Content(url);
+        }
+
+        public IActionResult Check(string name = "", string platform = "", string vesion = "1.0.0", string ver = "1.0.0", string uid = "")
+        {
+            var packages = _versionSettingsService.GetPackages(name);
+            var groups = _versionSettingsService.GetGroups();
+            var router = new Router(packages, groups);
+            var item = router.Match(platform, vesion, uid);
+
+            if (item != null)
+            {
+                Version runVersion;
+                if (Version.TryParse(ver, out runVersion))
+                {
+                    var fullVersion = new Version(
+                         runVersion.Major >= 0 ? runVersion.Major : 0,
+                         runVersion.Minor >= 0 ? runVersion.Minor : 0,
+                         runVersion.Build >= 0 ? runVersion.Build : 0,
+                         runVersion.Revision >= 0 ? runVersion.Revision : 0);
+
+                    if (fullVersion < item.PackageVersion)
+                        return Content(item.Url);
+                }
+            }
+            return Content("");
         }
 
         public IActionResult Error()
